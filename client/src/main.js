75f85a2bc6430da3a24f8ec013fd0c982fca83d9
abaze91 <abaze_aftab@hotmail.com>
2022-03-04 19:32:13 +0100

    add favicon + pwa plugins

--- client/src/main.js
+++ client/src/main.js
@@ -3,5 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./registerServiceWorker";
 
 createApp(App).use(store).use(router).mount("#app");
